ed08486eb09edc0bb9013d47ab1b1d9286c0a793
Akkireddy8 <156268619+Akkireddy8@users.noreply.github.com>
2024-12-04 22:17:04 -0600

    Revert "Updated project"
    This reverts commit 0c6d30ab847c375350780a56d6267d6f1340f8c1.

--- app/src/main/java/com/org/tunestream/interfaces/OnClickInterface.java
+++
@@ -1,9 +0,0 @@
-package com.org.tunestream.interfaces;
-
-import com.google.protobuf.Any;
-import com.org.tunestream.models.Playlist;
-import com.org.tunestream.models.Song;
-
-public interface OnClickInterface {
-    void onItemClick(Playlist playlist);
-}
--- app/src/main/java/com/org/tunestream/interfaces/OnSongClickInterface.java
+++
@@ -1,7 +0,0 @@
-package com.org.tunestream.interfaces;
-
-import com.org.tunestream.models.Song;
-
-public interface OnSongClickInterface {
-    void onItemClick(Song playlist);
-}
