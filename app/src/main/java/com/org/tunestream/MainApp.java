eec39756be692c03730356ca2c9b76dba799c163
Akkireddy8 <156268619+Akkireddy8@users.noreply.github.com>
2024-12-07 16:21:20 -0600

    updated java

--- app/src/main/java/com/org/tunestream/MainApp.java
+++ app/src/main/java/com/org/tunestream/MainApp.java
@@ -1,47 +1,19 @@
 package com.org.tunestream;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
-
 import android.app.Application;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Handler;
-import android.os.Looper;
-
-import androidx.annotation.NonNull;
-import androidx.lifecycle.ViewModelStore;
-import androidx.lifecycle.ViewModelStoreOwner;
 
 import com.org.tunestream.auth.views.LoginActivity;
+import com.org.tunestream.firebase_manager.UserDefaultsManager;
 import com.org.tunestream.home.HomeActivity;
 
-public class MainApp extends Application implements ViewModelStoreOwner {
-
-    private ViewModelStore appViewModelStore;
-
+public class MainApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        appViewModelStore = new ViewModelStore();
-        SharedPreferences sharedPreferences = getSharedPreferences("user_preferences", MODE_PRIVATE);
-        String email = sharedPreferences.getString("email", "");
-        checkLogin(email);
-    }
-
-    private void checkLogin(String email) {
-        Intent intent;
-        if (email.isEmpty()) {
-            intent = new Intent(this, LoginActivity.class);
-        } else {
-            intent = new Intent(this, HomeActivity.class);
-        }
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-
-    @NonNull
-    @Override
-    public ViewModelStore getViewModelStore() {
-        return appViewModelStore;
+        /*UserDefaultsManager userDefaultsManager = UserDefaultsManager.getInstance(getBaseContext());
+        if (!userDefaultsManager.isLoggedIn())
+            startActivity(new Intent(getBaseContext(), LoginActivity.class));
+        else startActivity(new Intent(getBaseContext(), HomeActivity.class));*/
     }
 }
